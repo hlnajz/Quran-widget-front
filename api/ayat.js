ac665d6811bf2f2a95e7c38147a94cdb82f41028
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2025-02-21 01:35:31 +0100

    old

--- api/ayat.js
+++ api/ayat.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { createCanvas, registerFont, Image } = require("canvas");
 const path = require("path");
 
-// Amiri font for Arabic text
+// Amiri font for arabic text
 registerFont(path.join(__dirname, "../public/fonts/Amiri-Regular.ttf"), {
   family: "Amiri",
 });
@@ -44,46 +44,6 @@ function wrapText(text, maxWordsPerLine) {
   return lines;
 }
 
-// Theme colors
-const themes = {
-  dark: {
-    borderColor: "#FF5555",
-    footerColor: "#FF5555",
-    bgColor: "#1a1a1d",
-    textColor: "#ffffff",
-  },
-  light: {
-    borderColor: "#00BFFF",
-    footerColor: "#00BFFF",
-    bgColor: "#ffffff",
-    textColor: "#000000",
-  },
-  ocean: {
-    borderColor: "#0077B6",
-    footerColor: "#0096C7",
-    bgColor: "#CAF0F8",
-    textColor: "#03045E",
-  },
-  forest: {
-    borderColor: "#228B22",
-    footerColor: "#32CD32",
-    bgColor: "#E0F2F1",
-    textColor: "#004D40",
-  },
-  sunset: {
-    borderColor: "#FF4500",
-    footerColor: "#FF6347",
-    bgColor: "#FFF5E1",
-    textColor: "#8B0000",
-  },
-  royal: {
-    borderColor: "#6A0DAD",
-    footerColor: "#8A2BE2",
-    bgColor: "#F3E5F5",
-    textColor: "#4B0082",
-  },
-};
-
 // Route 1: Return Random Ayah as JSON
 module.exports.json = async (req, res) => {
   const ayatData = await getAyatData();
@@ -107,7 +67,7 @@ module.exports.image = async (req, res) => {
   const canvasWidth = parseInt(width, 10);
   const canvasHeight = parseInt(height, 10);
 
-  // Styling variables and size of fonts parameters
+  // styiling variables and size of fonts parameters
   const padding = 20;
   const lineSpacing = 25;
   const borderWidth = 20;
@@ -116,8 +76,8 @@ module.exports.image = async (req, res) => {
   const sectionMarginBottom = 20;
   const ayatData = await getAyatData();
 
-  const footerColor = themes[theme].footerColor;
-  const borderColor = themes[theme].borderColor;
+  const footerColor = theme === "dark" ? "#FF5555" : "#00BFFF";
+  const borderColor = theme === "dark" ? "#FF5555" : "#00BFFF";
 
   if (ayatData) {
     const randomAyah = getRandomAyah(ayatData);
@@ -135,7 +95,7 @@ module.exports.image = async (req, res) => {
     ctx.fillStyle = borderColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = themes[theme].bgColor;
+    ctx.fillStyle = theme === "dark" ? "#1a1a1d" : "#ffffff";
     ctx.fillRect(
       borderWidth,
       borderWidth,
@@ -148,7 +108,7 @@ module.exports.image = async (req, res) => {
 
     // Arabic Ayah
     ctx.font = `${fontSize}px Amiri`;
-    ctx.fillStyle = themes[theme].textColor;
+    ctx.fillStyle = theme === "dark" ? "#ffffff" : "#000000";
     ctx.textAlign = "right";
     let yOffset = sectionMarginTop + borderWidth;
     ctx.fillText(randomAyah.text.arabic, endX, yOffset);
@@ -185,7 +145,7 @@ module.exports.image = async (req, res) => {
 
     yOffset += 2 + sectionMarginBottom;
 
-    // English Hadith
+    //English Hadith
     ctx.font = `${fontSize * 0.8}px Arial`;
     ctx.textAlign = "left";
     englishHadithLines.forEach((line) => {
@@ -195,7 +155,7 @@ module.exports.image = async (req, res) => {
 
     yOffset += 2 + sectionMarginBottom;
 
-    // Background Logo
+    // background
     const logoPath = path.join(__dirname, "../public/bga.png");
     const logo = new Image();
     logo.onload = () => {
@@ -233,5 +193,3 @@ module.exports.image = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch Quran Sunnah data" });
   }
 };
-
-// hlnajz was here light
